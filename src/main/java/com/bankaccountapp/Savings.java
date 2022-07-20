3dc4133ccbca860c9a38118680c303c478e9d9fc
HaozheWang <haozhe.wang.chn@outlook.com>
2022-07-20 01:28:49 -0400

    Account Archit

--- src/main/java/com/bankaccountapp/Savings.java
+++ src/main/java/com/bankaccountapp/Savings.java
@@ -2,11 +2,19 @@ package com.bankaccountapp;
 
 public class Savings extends Account{
     // List properties specific to a Saving account
+    int safetyDepositBoxID;
+    int safetyDepositBoxKey;
 
     // Constructor to initialize Savings account properties settings
-    public Savings(String name){
-        super(name);    // Account is the parent class that super refers to
-        System.out.println("New Savings Account");
+    public Savings(String name, String sIN, double initDeposit){
+        super(name, sIN, initDeposit);    // Account is the parent class that super refers to
+        accountNumber = "2" + accountNumber;    // 2 refers to savings account
+        // System.out.println("NEW SAVINGS ACCOUNT: ACCOUNT NUM: " + accountNumber);
     }
     // List any methods specific to the savings account
+    public void showInfo(){
+        super.showInfo();
+        System.out.println("ACCOUNT TYPE: SAVINGS");
+        System.out.println("************");
+    }
 }
