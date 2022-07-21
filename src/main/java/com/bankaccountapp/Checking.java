a40b0d2bfb82d740a63cf3b2a884ae4b39834d41
HaozheWang <haozhe.wang.chn@outlook.com>
2022-07-20 21:24:54 -0400

    account setting & bonus

--- src/main/java/com/bankaccountapp/Checking.java
+++ src/main/java/com/bankaccountapp/Checking.java
@@ -10,11 +10,23 @@ public class Checking extends Account{
         super(name, sIN, initDeposit);    // Account is the parent class that super refers to
         accountNumber = "1" + accountNumber;  // 1 refers to checking account
         // System.out.println("NEW CHECKING ACCOUNT: ACCOUNT NUM: " + accountNumber);
+        setDebitCard();
     }
+
+    private void setDebitCard(){
+        debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+        debitCardPIN = (int) (Math.random() * Math.pow(10, 4));
+    }
+
     // List any methods specific to the checking account
     public void showInfo(){
-        super.showInfo();
         System.out.println("ACCOUNT TYPE: CHECKING");
+        super.showInfo();
+        System.out.println(
+                "CHECKING ACCOUNT FEATURES: " +
+                "\nDEBIT CARD NUMBER: " + this.debitCardNumber +
+                "\nDEBIT CARD PIN: " + this.debitCardPIN
+        );
         System.out.println("************");
     }
 }
